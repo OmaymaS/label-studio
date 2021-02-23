067525a2807acdb435c5caeb69cb7efc89444f3c
OmaymaS <13340411+OmaymaS@users.noreply.github.com>
2021-02-23 14:56:48 +0100

    Update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 label-studio start-multi-session  ${PROJECT_NAME:-my_project} -b \
-             --initial-project-description 'multi session project' \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8081} --protocol ${PROTOCOL:-http://} \
              --username ${USERNAME:-""} --password ${PASSWORD:-""} ${INIT_COMMAND:-"--init"}
