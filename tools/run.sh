58f3ff87c53f6d7c050baeda4a95d27eab01d28f
OmaymaS <13340411+OmaymaS@users.noreply.github.com>
2021-02-23 14:44:06 +0100

    Update run.sh

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-label-studio start-multi-session --root-dir ./ ${PROJECT_NAME:-my_project} -b \
+label-studio start-multi-session  ${PROJECT_NAME:-my_project} -b \
+             --initial-project-description 'multi session project' \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8081} --protocol ${PROTOCOL:-http://} \
              --username ${USERNAME:-""} --password ${PASSWORD:-""} ${INIT_COMMAND:-"--init"}
