eccf0ba92a3bc417f243cad4bf726e072ff41e08
osaid <13340411+OmaymaS@users.noreply.github.com>
2021-02-23 13:51:25 +0100

    edit start-multi-session

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+mkdir ./session_projects
 
-label-studio start-multi-session  ${PROJECT_NAME:-my_project} -b \
+label-studio start-multi-session --root-dir ./session_projects ${PROJECT_NAME:-my_project} -b \
              --host ${HOST:-0.0.0.0} --port ${PORT:-8081} --protocol ${PROTOCOL:-http://} \
              --username ${USERNAME:-""} --password ${PASSWORD:-""} ${INIT_COMMAND:-"--init"}
